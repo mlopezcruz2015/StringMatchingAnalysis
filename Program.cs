d67a53da14bc510aaf6107b09fa99ee9c8ba3c53
michael lopez <mlopezcruz2015@fau.edu>
2021-04-20 21:47:25 -0400

    Finalized code.

--- Program.cs
+++ Program.cs
@@ -5,17 +5,30 @@ using System.Linq;
 
 namespace StringMatchingAnalysis
 {
-
     class Program
     {
+        //Global variables to save the total sum of the timer and then get average.
+        private static double boyerRandomPatternAverage = 0;
+        private static double boyerContainedPatternAverage = 0;
+        private static double bruteRandomPatternAverage = 0;
+        private static double bruteContainedPatternAverage = 0;
+
         static void Main(string[] args)
         {
-            if (File.Exists("Brute.txt"))
-                File.Delete("Brute.txt");
+            //Reset Logs
+            if (File.Exists("BruteRandom.txt"))
+                File.Delete("BruteRandom.txt");
 
-            if (File.Exists("Boyer.txt"))
-                File.Delete("Boyer.txt");
+            if (File.Exists("BoyerRandom.txt"))
+                File.Delete("BoyerRandom.txt");
 
+            if (File.Exists("BruteContained.txt"))
+                File.Delete("BruteContained.txt");
+
+            if (File.Exists("BoyerContained.txt"))
+                File.Delete("BoyerContained.txt");
+
+            //Use a switch statement to set 'n'
             for (int i = 0; i < 5; i++)
             {
                 int n = 0;
@@ -41,178 +54,241 @@ namespace StringMatchingAnalysis
                         break;
                 }
 
-                //Generate String of Text.
+                //Generate Random String of Text.
                 string text = RandomString(n);
 
-                int m = 1000;
+
                 string randomPattern = "";
                 string containedPattern = "";
 
-                //Run first two tests with m = 1000
-                randomPattern = RandomString(m);
-                containedPattern = GenerateContainedPattern(text, m);
+                int m = 1000;
 
-                File.AppendAllText("Brute.txt", $"n = {n}, m = {m}, with Random String" + Environment.NewLine);
-                BruteMatching(text, randomPattern);
+                //Run tests 10000 times
+                for (int k = 0; k < 10000; k++)
+                {
+                    //Run first tests with m = 1000
+                    randomPattern = RandomString(m);
+                    containedPattern = GenerateContainedPattern(text, m);
 
-                File.AppendAllText("Brute.txt", $"n = {n}, m = {m}, with Generated Matching String" + Environment.NewLine);
-                BruteMatching(text, containedPattern);
+                    BruteMatching(text, randomPattern, false);
 
-                File.AppendAllText("Boyer.txt", $"n = {n}, m = {m}, with Random String" + Environment.NewLine);
-                BoyerMoore(text, randomPattern);
+                    BruteMatching(text, containedPattern, true);
 
-                File.AppendAllText("Boyer.txt", $"n = {n}, m = {m}, with Generated Matching String" + Environment.NewLine);
-                BoyerMoore(text, containedPattern);
+                    BoyerMoore(text, randomPattern, false);
 
-                //Run Second two tests with m = n / 2
+                    BoyerMoore(text, containedPattern, true);
+                }
+
+                LogResults(n, m);
+
+                //Run Second set of tests with m = n / 2
                 m = n / 2;
-                randomPattern = RandomString(m);
-                containedPattern = GenerateContainedPattern(text, m);
 
-                File.AppendAllText("Brute.txt", $"n = {n}, m = {m}, with Random String" + Environment.NewLine);
-                BruteMatching(text, randomPattern);
+                //Run tests 10000 times
+                for (int k = 0; k < 10000; k++)
+                {
+                    randomPattern = RandomString(m);
+                    containedPattern = GenerateContainedPattern(text, m);
 
-                File.AppendAllText("Brute.txt", $"n = {n}, m = {m}, with Generated Matching String" + Environment.NewLine);
-                BruteMatching(text, containedPattern);
+                    BruteMatching(text, randomPattern, false);
 
-                File.AppendAllText("Boyer.txt", $"n = {n}, m = {m}, with Random String" + Environment.NewLine);
-                BoyerMoore(text, randomPattern);
+                    BruteMatching(text, containedPattern, true);
 
-                File.AppendAllText("Boyer.txt", $"n = {n}, m = {m}, with Generated Matching String" + Environment.NewLine);
-                BoyerMoore(text, containedPattern);
+                    BoyerMoore(text, randomPattern, false);
+
+                    BoyerMoore(text, containedPattern, true);
+                }
+
+                LogResults(n, m);
             }
         }
 
-        private static void BruteMatching(string T, string P)
+        /// <summary>
+        /// Uses the global variables that store the average running time for each algorithm. Writes these average RTs to .txt files.
+        /// </summary>
+        /// <param name="n">Text length</param>
+        /// <param name="m">Pattern length</param>
+        private static void LogResults(int n, int m)
         {
-            //Run 10 times and get average
-            double average = 0;
-            double totalSum = 0;
-            int indexFound = -1;
+            //Get Average
+            boyerRandomPatternAverage = boyerRandomPatternAverage / 10000;
+            boyerContainedPatternAverage = boyerContainedPatternAverage / 10000;
+            bruteRandomPatternAverage = bruteRandomPatternAverage / 10000;
+            bruteContainedPatternAverage = bruteContainedPatternAverage / 10000;
 
-            for (int x = 0; x < 1000; x++)
-            {
-                //Start Timer
-                var stopwatch = new Stopwatch();
-                stopwatch.Start();
+            File.AppendAllText("BruteRandom.txt", $"n = {n}, m = {m}, with Random String" + Environment.NewLine);
+            File.AppendAllText("BruteRandom.txt", $"Average Time elapsed: {bruteRandomPatternAverage}msec" + Environment.NewLine + Environment.NewLine);
 
-                int n = T.Length;
-                int m = P.Length;
+            File.AppendAllText("BruteContained.txt", $"n = {n}, m = {m}, with Generated Matching String" + Environment.NewLine);
+            File.AppendAllText("BruteContained.txt", $"Average Time elapsed: {bruteContainedPatternAverage}msec" + Environment.NewLine + Environment.NewLine);
 
-                for (int i = 0; i < n - m; i++)
-                {
-                    int j = 0;
+            File.AppendAllText("BoyerRandom.txt", $"n = {n}, m = {m}, with Random String" + Environment.NewLine);
+            File.AppendAllText("BoyerRandom.txt", $"Average Time elapsed: {boyerRandomPatternAverage}msec" + Environment.NewLine + Environment.NewLine);
 
-                    while (j < m && T[i + j] == P[j])
-                    {
-                        j++;
-                    }
+            File.AppendAllText("BoyerContained.txt", $"n = {n}, m = {m}, with Generated Matching String" + Environment.NewLine);
+            File.AppendAllText("BoyerContained.txt", $"Average Time elapsed: {boyerContainedPatternAverage}msec" + Environment.NewLine + Environment.NewLine);
 
-                    if (j == m)
-                    {
-                        indexFound = i;
-                        Console.WriteLine($"Match string found at index: {indexFound}");
-                        break;
-                    }
-                }
-
-                //End Timer, get time.
-                stopwatch.Stop();
-                TimeSpan timeTaken = stopwatch.Elapsed;
-                var elapsed_time = (double)timeTaken.TotalMilliseconds;
-                totalSum += elapsed_time;
-
-
-                //Console.WriteLine($"Run {x}, RT = {elapsed_time}");
-            }
-
-
-            average = totalSum / 1000;
-            average = Math.Round((double)average, 3);
-
-            if (indexFound != -1)            
-                File.AppendAllText("Brute.txt", $"Match found." + Environment.NewLine);            
-            else            
-                File.AppendAllText("Brute.txt", $"Match not found." + Environment.NewLine);            
-
-            File.AppendAllText("Brute.txt", $"Average Time elapsed: {average}msec" + Environment.NewLine + Environment.NewLine);
+            //Reset Global Variables
+            boyerRandomPatternAverage = 0;
+            boyerContainedPatternAverage = 0;
+            bruteRandomPatternAverage = 0;
+            bruteContainedPatternAverage = 0;
         }
 
-        private static void BoyerMoore(string T, string P)
+        /// <summary>
+        /// Runs the Brute Matching algorithm for string matching
+        /// </summary>
+        /// <param name="T">String of Text</param>
+        /// <param name="P">Pattern to be found</param>
+        /// <param name="containedPattern">Marker to determine what global variable to write to.</param>
+        private static void BruteMatching(string T, string P, bool containedPattern)
         {
-            //Run 10 times and get average
-            double average = 0;
             double totalSum = 0;
-            int indexFound = -1;
 
-            for (int x = 0; x < 1000; x++)
+            //Start Timer
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int n = T.Length;
+            int m = P.Length;
+
+            for (int i = 0; i < n - m; i++)
             {
-                int n = T.Length;
-                int m = P.Length;
+                int j = 0;
 
-                int[] shiftIndex = new int[n];
-                shift(P.ToCharArray(), m, shiftIndex);
-
-                //Start Timer
-                var stopwatch = new Stopwatch();
-                stopwatch.Start();
-
-                for (int i = 0; i <= n - m;)
+                while (j < m && T[i + j] == P[j])
                 {
-                    int pos = m - 1;
-
-                    while (pos >= 0 && P.Substring(pos, 1) == T.Substring(i + pos, 1))
-                        pos--;
-
-                    if (pos < 0)
-                    {
-                        indexFound = i;
-
-                        Console.WriteLine($"Match string found at index: {indexFound}");
-
-                        i += (i + m < n) ? m - shiftIndex[T[i + m]] : 1;
-
-                        break;
-                    }
-                    else
-                    {
-                        i += Max(1, pos - shiftIndex[T[i + pos]]);
-                    }
+                    j++;
                 }
 
-                //End Timer, get time.
-                stopwatch.Stop();
-                TimeSpan timeTaken = stopwatch.Elapsed;
-                var elapsed_time = (double)timeTaken.TotalMilliseconds;
-                totalSum += elapsed_time;
+                if (j == m)
+                {
+                    Console.WriteLine($"Brute: index: {i}");
+                    break;
+                }
             }
 
+            //End Timer, get time.
+            stopwatch.Stop();
+            TimeSpan timeTaken = stopwatch.Elapsed;
+            var elapsed_time = (double)timeTaken.TotalMilliseconds;
+            totalSum += elapsed_time;
 
-            average = totalSum / 1000;
-            average = Math.Round((double)average, 3);
-
-            if (indexFound != -1)
-                File.AppendAllText("Boyer.txt", $"Match found." + Environment.NewLine);
+            //Save elapsed time to appropriate variable.
+            if (containedPattern)
+            {
+                bruteContainedPatternAverage += totalSum;
+            }
             else
-                File.AppendAllText("Boyer.txt", $"Match not found." + Environment.NewLine);
+            {
+                bruteRandomPatternAverage += totalSum;
+            }
+        }
 
-            File.AppendAllText("Boyer.txt", $"Average Time elapsed: {average}msec" + Environment.NewLine + Environment.NewLine);
+        /// <summary>
+        /// Runs the Boyer-Moore algorithm for string matching
+        /// </summary>
+        /// <param name="T">String of Text</param>
+        /// <param name="P">Pattern to be found</param>
+        /// <param name="containedPattern">Marker to determine what global variable to write to.</param>
+        private static void BoyerMoore(string T, string P, bool containedPattern)
+        {
+            double totalSum = 0;
+
+            int n = T.Length;
+            int m = P.Length;
+
+            int[] shiftIndex = new int[n];
+            shift(P.ToCharArray(), m, shiftIndex, n);
+
+            //Start Timer
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 0; i <= n - m;)
+            {
+                int pos = m - 1;
+
+                while (pos >= 0 && P[pos] == T[i + pos])
+                    pos--;
+
+                if (pos < 0)
+                {
+                    Console.WriteLine($"Boyer index: {i}");
+
+                    i += (i + m < n) ? m - shiftIndex[T[i + m]] : 1;
+
+                    break;
+                }
+                else
+                {
+                    i += Max(1, pos - shiftIndex[T[i + pos]]);
+                }
+            }
+
+            //End Timer, get time.
+            stopwatch.Stop();
+            TimeSpan timeTaken = stopwatch.Elapsed;
+            var elapsed_time = (double)timeTaken.TotalMilliseconds;
+            totalSum += elapsed_time;
+
+            //Save elapsed time to appropriate variable.
+            if (containedPattern)
+            {
+                boyerContainedPatternAverage += totalSum;
+            }
+            else
+            {
+                boyerRandomPatternAverage += totalSum;
+            }
+        }
+
+        //used for booyer moore
+        static void shift(char[] str, int m, int[] tempShift, int n)
+        {
+            int i;
+
+            // Initialize all occurrences as -1
+            for (i = 0; i < n; i++)
+                tempShift[i] = -1;
+
+            // Fill the actual value of last occurrence 
+            // of a character
+            for (i = 0; i < m; i++)
+                tempShift[(int)str[i]] = i;
         }
 
         private static Random random = new Random();
 
-        public static string RandomString(int length)
+        /// <summary>
+        /// Creates random string based on the length passed in.
+        /// </summary>
+        /// <param name="length">Length of string</param>
+        /// <returns>Randomized string of length l</returns>
+        public static string RandomString(int l)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
+            return new string(Enumerable.Repeat(chars, l)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
+        /// <summary>
+        /// Gets max between two numbers.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>Max of two numbers</returns>
         public static int Max(int a, int b)
         {
             return a > b ? a : b;
         }
 
+        /// <summary>
+        /// Generates a pattern that is contained within a string of text.
+        /// </summary>
+        /// <param name="text">String of text from which pattern is generated</param>
+        /// <param name="m">length of pattern to create</param>
+        /// <returns>pattern substring from string of text</returns>
         public static string GenerateContainedPattern(string text, int m)
         {
             int maxIndex = text.Length - m;
@@ -222,21 +298,6 @@ namespace StringMatchingAnalysis
             string containedPattern = text.Substring(chosenIndex, m);
 
             return containedPattern;
-        }
-
-        //used for booyer moore
-        static void shift(char []str, int m, int []tempShift)
-        {
-            int i;
-
-            // Initialize all occurrences as -1
-            for (i = 0; i < str.Length; i++)
-                tempShift[i] = -1;
-
-            // Fill the actual value of last occurrence 
-            // of a character
-            for (i = 0; i < m; i++)
-                tempShift[(int)str[i]] = i;
         }
     }
 }
